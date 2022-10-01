c32286d223065094e290e25a9a4a6fa0faf8c3ce
Boudissa Ihab <boudissa.ihab@gmail.com>
2022-10-02 00:21:54 +0100

    client-side main page

---
+++ app/Http/Livewire/Client/Home.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Livewire\Client;
+
+use App\Models\Category;
+use App\Models\ContactUs;
+use App\Models\Project;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+use Throwable;
+
+class Home extends Component
+{
+    public function getCategoriesProperty()
+    {
+        return Category::get();
+    }
+
+    public function getProjectsProperty()
+    {
+        return Project::with('categories')->get();
+    }
+
+    /*** Contact us ***/
+    public $name;
+    public $email;
+    public $subject;
+    public $message;
+
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'subject' => 'required|string',
+            'message' => 'required|string'
+        ];
+    }
+
+    protected $messages = [
+        'email.required' => "L'adresse mail ne peut pas être vide",
+        'email.email' => "Veuillez entrer une adresse mail valide",
+        'subject.required' => "Le sujet du message ne peut pas être vide",
+        'message.required' => "Le contenu du message ne peut pas être vide"
+    ];
+
+    public function updated($property)
+    {
+        $this->validateOnly($property);
+    }
+
+    public function send()
+    {
+        if ($this->validate()) {
+            try {
+                DB::transaction(function () {
+                    ContactUs::create([
+                        'name' => $this->name ?? "",
+                        'email' => $this->email,
+                        'subject' => $this->subject,
+                        'message' => $this->message,
+                        'read_at' => null
+                    ]);
+                    session()->flash("success", "Message envoyé avec succès !");
+                });
+            } catch (Throwable $e) {
+                session()->flash("error", "Impossible d'envoyer ce message, veuillez réessayer");
+            }
+        }
+    }
+    /*** Contact us ***/
+
+    public function render()
+    {
+        return view('livewire.client.home')->layout('layouts.client');
+    }
+}
